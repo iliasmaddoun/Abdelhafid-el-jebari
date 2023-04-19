4353920fefa5a7a793cbb44909e31d0366d5e929
iliasmaddoun <59096279+iliasmaddoun@users.noreply.github.com>
2023-04-19 02:26:07 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Automatisierungstechniker","Mechatroniker","Fachkundige Wartung"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
